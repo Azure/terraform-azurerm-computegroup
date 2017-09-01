a7dd9b0c0dce6ce885ebc7955b7a04beebe2a000
dcaro <dcaro@Damiens-MacBook-Pro.local>
2017-08-31 19:24:22 -0700

    removing unnecessary comments in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,9 @@
 output "vmss_name_public" {
   value = "${var.vm_os_simple == "Windows" ? azurerm_virtual_machine_scale_set.vm-windows.name : azurerm_virtual_machine_scale_set.vm-linux.name}"
-  # value = "${azurerm_virtual_machine_scale_set.vm-linux.name}"
 }
 
 output "vmss_id" {
   value = "${var.vm_os_simple == "Windows" ? azurerm_virtual_machine_scale_set.vm-windows.id : azurerm_virtual_machine_scale_set.vm-linux.id }}" 
-  # value = "${azurerm_virtual_machine_scale_set.vm-linux.name}"
 }
 
 output "vnet_id" {
