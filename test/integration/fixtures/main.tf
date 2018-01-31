5e35030de9d45e5c1d1228411108d1232031f1ed
Vaijanath Angadihiremath <vaangadi@microsoft.com>
2018-01-31 22:03:49 +0000

    Fixing the E2E test

--- test/integration/fixtures/main.tf
+++ test/integration/fixtures/main.tf
@@ -3,12 +3,14 @@ provider "azurerm" {
 }
 
 module "network" {
+  version             = "1.1.1"
   source              = "Azure/network/azurerm"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
 }
 
 module "loadbalancer" {
+  version             = "1.0.0"
   source              = "Azure/loadbalancer/azurerm"
   resource_group_name = "${var.resource_group_name}"
   location            = "${var.location}"
@@ -32,11 +34,7 @@ module "computegroup" {
   vnet_subnet_id                         = "${module.network.vnet_subnets[0]}"
   load_balancer_backend_address_pool_ids = "${module.loadbalancer.azurerm_lb_backend_address_pool_id}"
   cmd_extension                          = "sudo apt-get -y install nginx"
-
-  lb_port = {
-    http  = ["80", "Tcp", "80"]
-    https = ["443", "Tcp", "443"]
-  }
+  
 
   tags = {
     environment = "dev"
--- test/integration/fixtures/variables.tf
+++ test/integration/fixtures/variables.tf
@@ -6,3 +6,8 @@ variable "resource_group_name" {
 }
 
 variable "vm_os_simple" {}
+
+variable "vnet_subnet_id" {}
+
+variable "load_balancer_backend_address_pool_ids" {}
+
